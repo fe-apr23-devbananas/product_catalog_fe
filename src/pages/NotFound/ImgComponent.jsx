e032bc48f2cbad222f0be978684cdca8969f235f
Viktor Panchenko <panch.v1811@gmail.com>
2023-07-31 14:03:25 +0200

    not-found-page w/ lazy loading

---
+++ src/pages/NotFound/ImgComponent.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import { Blurhash } from 'react-blurhash';
+
+export default function ImgComponent({ src }) {
+	const [imgLoaded, setImgLoaded] = useState(false);
+  const imgSize = '320px';
+
+	useEffect(() => {
+		const img = new Image();
+		img.onload = () => {
+			setImgLoaded(true);
+		};
+		img.src = src;
+	}, [src]);
+
+	return (
+		<div>
+			<div style={{ display: imgLoaded ? 'none' : 'inline', }}>
+				<Blurhash
+					hash='LuP%9hxt%Lxtt7j@j@oe_LRkM|Rk'
+					width={imgSize}
+					height={imgSize}
+					resolutionX={128}
+					resolutionY={128}
+					punch={1}
+				/>
+			</div>
+
+				<img
+					src={src}
+					alt='Page Not Found'
+          style={{
+            display: !imgLoaded ? 'none' : 'inline',
+            width: imgSize,
+            height: imgSize,
+          }}
+				/>
+		</div>
+	);
+}
