8a6c9052cadc01a90dde10c268c8dc19baf7eea3
Maryan Mats <cholgyni@gmail.com>
2023-08-01 10:00:12 +0300

    footer-adaptive-layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import { Footer } from './components/Footer/Footer';
 
 export const App = () => {
   return (
     <div className="App">
-      Nothing
+      <Footer />
     </div>
   );
 };
