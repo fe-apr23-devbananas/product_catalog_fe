df63d31dd7bad038fb80f1a1ca9bf5845354bb0e
Mykola Maik <mykoluamaik@gmail.com>
2023-08-02 17:01:25 +0300

    create reusable custom hook for load phones list from API

--- src/pages/PhonesPage.tsx
+++ src/pages/PhonesPage.tsx
@@ -1,3 +1,18 @@
 import React, { FC } from 'react';
+import { Card } from '../components/Card/Card';
+import { Phone } from '../types/Phone';
+import { useFetchData } from '../hooks/useFetchData';
 
-export const PhonesPage: FC = () => <main>Phones</main>;
+export const PhonesPage: FC = () => {
+  const { isLoading, data: phones } = useFetchData<Phone>();
+
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        phones.map((phone: Phone) => <Card key={phone.id} product={phone} />)
+      )}
+    </div>
+  );
+};
