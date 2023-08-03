7d34aa004c49a8d36c54f7574a5ef13faa1f03b8
Mykola Maik <mykoluamaik@gmail.com>
2023-08-03 23:47:49 +0300

    create counter component and Added button

--- src/components/Buttons/Buttons.tsx
+++ src/components/Buttons/Buttons.tsx
@@ -1,11 +1,26 @@
 import { Button, IconButton } from '@mui/material';
-import './Buttons.scss';
 import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
 import React, { FC } from 'react';
 
 export const Buttons: FC = () => {
   return (
     <div className="buttons">
+      {/* <Button
+        variant="contained"
+        className="buttons__buy"
+        size="medium"
+        style={{
+          background: '#fff',
+          borderRadius: '48px',
+          transition: 'all 0.5s linear 0s',
+          fontWeight: '700',
+          lineHeight: '21px',
+          border: '1px solid #E2E6E9',
+          color: '#4219D0'
+        }}
+      >
+        Added
+      </Button> */}
       <Button
         variant="contained"
         className="buttons__buy"
---
+++ src/components/Header/Counter/Counter.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import './Counter.scss';
+
+export const Counter: FC = () => (
+  <div className="counter">
+    <span className="counter__text">14</span>
+  </div>
+);
