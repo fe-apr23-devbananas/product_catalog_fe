2bf5af1541012ab811cd65ba27e8ecd8c1bd2c46
Kateryna Sokur <k.o.sokur@gmail.com>
2023-08-04 12:30:30 +0200

    add card size fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.scss';
 import { Route, Routes } from 'react-router-dom';
-import { Layout } from './Layout';
+import { Layout } from './components/Layout/Layout';
 import { HomePage, PhonesPage, TabletsPage, AccessoriesPage } from './pages';
 import { NotFoundPage } from './pages/NotFoundPage';
 import { NotFoundRedirect } from './pages/NotFoundPage/NotFoundRedirect';
