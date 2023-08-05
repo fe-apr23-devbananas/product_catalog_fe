756d20ffdf34c766cf808a9e08cb692947eac8e1
Viktor Panchenko <panch.v1811@gmail.com>
2023-08-05 18:27:41 +0200

    ProductPage-About-component implemented

---
+++ src/components/About/About.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react';
+import './About.scss';
+import { ProductDetails } from '../../types/ProductDetails';
+import { generateId } from '../../helpers/generateId';
+
+//When inserting this component pass productInfo like this:
+// element={<About product={productInfo} />}
+
+interface Props {
+  product: ProductDetails;
+}
+
+export const About: FC<Props> = ({ product }) => {
+  return (
+    <div className="About">
+      {product.description.map((desc) => (
+        <div className="About__paragraph" key={generateId()}>
+          <p className="About__paragraphHeader">{desc.title}</p>
+          <p className="About__text">{desc.text}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
