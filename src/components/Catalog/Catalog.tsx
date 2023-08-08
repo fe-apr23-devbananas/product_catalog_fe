9bbb1521f63bd026902d331f5dd6321c13cc7e70
Kateryna Sokur <k.o.sokur@gmail.com>
2023-08-08 08:20:14 +0200

    add-catalog-title-total-items

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -10,7 +10,10 @@ interface Props {
 export const Catalog: React.FC<Props> = ({ products }) => {
   return (
     <div className="container">
-      <div className="catalog-grid">
+      <div className="breadcrumbs">there will be breadcrumbs</div>
+      <div className="catalog__title">Mobile phones</div>
+      <div className="catalog__total-models">{products.length} models</div>
+      <div className="catalog__grid">
         {products.map((product) => (
           <Card key={product.id} product={product} />
         ))}
