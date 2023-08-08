2ffb302c371449df217ab43947d28152fa6a7e50
Kateryna Sokur <k.o.sokur@gmail.com>
2023-08-08 10:00:02 +0200

    add layout fix

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -10,9 +10,6 @@ interface Props {
 export const Catalog: React.FC<Props> = ({ products }) => {
   return (
     <div className="container">
-      <div className="breadcrumbs">there will be breadcrumbs</div>
-      <div className="catalog__title">Mobile phones</div>
-      <div className="catalog__total-models">{products.length} models</div>
       <div className="catalog__grid">
         {products.map((product) => (
           <Card key={product.id} product={product} />
